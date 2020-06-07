56c5a57cdeb575d003f745a03f33b4cd6492ddb3
Philippe Négrel-Jerzy <keltapolis@gmail.com>
2020-06-07 11:29:12 +0200

    small improvements to publish.bat

--- publish.bat
+++ publish.bat
@@ -1,17 +1,30 @@
 @echo off
 @rem if this fails, try : python -m pip install --user --upgrade setuptools wheel
-echo CWD: %cd%
+powershell write-host -fore Cyan CWD: %cd%
 if %ERRORLEVEL% neq 0 goto ProcessError
     python setup.py sdist bdist_wheel 
 :Follow
+powershell write-host -fore Cyan "Checking package wheel"
 twine check dist/*
 pip install -e .
-echo try the installed version on a third-party program be4 commiting, don't be stupid, this is important
-pause
-echo then provide username and password
+powershell write-host -fore Cyan "The program will pause now. Please test the new package before publishing it."
+python test.py
+set /p DUMMY=Press ENTER to continue...
+powershell write-host -fore Cyan "please provide username and password for pypi"
 twine upload dist/* 
+powershell write-host -fore Cyan "reinstalling from pypi"
+pip uninstall pconsole
+pip install pconsole
+powershell write-host -fore Green "done"
+powershell write-host -fore Cyan "showing info"
+pip show pconsole
 exit /b 0
 
 :ProcessError
+powershell write-host -fore Red "Process failed, redirecting..."
 python -m pip install --user --upgrade setuptools wheel
 goto Follow
+
+:ECHORED
+%Windir%\System32\WindowsPowerShell\v1.0\Powershell.exe write-host -foregroundcolor Red %1 
+goto:eof
