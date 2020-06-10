a00976bce0089c043418d4f90ef204d85336a1c4
Philippe Négrel-Jerzy <keltapolis@gmail.com>
2020-06-10 12:11:56 +0200

    Update publish.bat

--- publish.bat
+++ publish.bat
@@ -1,30 +1,48 @@
 @echo off
+cls
 @rem if this fails, try : python -m pip install --user --upgrade setuptools wheel
-powershell write-host -fore Cyan CWD: %cd%
-if %ERRORLEVEL% neq 0 goto ProcessError
-    python setup.py sdist bdist_wheel 
-:Follow
-powershell write-host -fore Cyan "Checking package wheel"
+
+set "cecho=powershell write-host -fore"
+%cecho% Cyan CWD: %cd% 
+
+@rem updating pip
+python -m pip install pip==19.0.1
+@rem try catch block
+python setup.py sdist bdist_wheel 
+if %ERRORLEVEL% neq 0 CALL:ProcessError1
+
+:Wheel
+%cecho% Cyan "Checking package wheel"
 twine check dist/*
-pip install -e .
-powershell write-host -fore Cyan "The program will pause now. Please test the new package before publishing it."
-python test.py
+if %ERRORLEVEL% neq 0 CALL:ProcessError2
+
+:Twine
+python -m pip install -e .
+%cecho% Cyan "The program will pause now. Please test the new package before publishing it."
+@python test.py
 set /p DUMMY=Press ENTER to continue...
-powershell write-host -fore Cyan "please provide username and password for pypi"
+%cecho% Cyan "please provide username and password for pypi"
 twine upload dist/* 
-powershell write-host -fore Cyan "reinstalling from pypi"
-pip uninstall pconsole
-pip install pconsole
-powershell write-host -fore Green "done"
-powershell write-host -fore Cyan "showing info"
-pip show pconsole
+%cecho% Cyan "reinstalling from pypi"
+python -m pip uninstall -y pconsole
+python -m pip install pconsole
+%cecho% Green "done"
+%cecho% Cyan "showing info"
+python -m pip show pconsole
+pause
 exit /b 0
 
-:ProcessError
-powershell write-host -fore Red "Process failed, redirecting..."
+:ProcessError1
+%cecho% Red "Process failed. Redirecting..."
 python -m pip install --user --upgrade setuptools wheel
-goto Follow
+goto:eof
 
-:ECHORED
+:ProcessError2
+%cecho% Red "Process failed. Redirecting..."
+python -m pip install twine
+goto:eof
+
+:ECHORED 
+@rem unused
 %Windir%\System32\WindowsPowerShell\v1.0\Powershell.exe write-host -foregroundcolor Red %1 
 goto:eof
